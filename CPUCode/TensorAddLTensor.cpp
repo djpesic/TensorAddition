ebcbe22d299db186c121865b600b9485fef426d5
djpesic <djpesic@yahoo.com>
2016-03-03 21:00:55 +0100

    Add addition of more than one tensor

--- CPUCode/TensorAddLTensor.cpp
+++ CPUCode/TensorAddLTensor.cpp
@@ -8,64 +8,64 @@
 using namespace std;
 static fstream input, output;
 
-double generateRandomNumber(){
-	double fMin=0, fMax=10000;
-	double f = (double)rand() / RAND_MAX;
+float generateRandomNumber(){
+	float fMin=0, fMax=10000;
+	float f = (float)rand() / RAND_MAX;
 	return fMin + f * (fMax - fMin);
 
 }
 
-static void addFirstRankTensors(int dim) {
+static void addFirstRankTensors(int dim, int num) {
 
-	Marray<double, 1> a1(dim), b1(dim), c1(dim);
+	Marray<float, 1> a(dim), sum(dim);
 
 	Index<'i'> i;
 	for (int ind = 0; ind < dim; ind++) {
-		input >> a1(ind);
+		input >> sum(ind);
+	}
+	num--;
+	for(int it=0;it<num;it++){
+		for (int ind = 0; ind < dim; ind++) {
+			input >> a(ind);
+		}
+		sum(i)=sum(i)+a(i);
 	}
 	for (int ind = 0; ind < dim; ind++) {
-		input >> b1(ind);
+		output << sum(ind) << " ";
 	}
-
-	c1(i) = a1(i) + b1(i);
-
-	for (int ind = 0; ind < dim; ind++) {
-		output << c1(ind) << " ";
-	}
-
-
 }
 
 
-static void addSecondRankTensors(int dim1, int dim2) {
-	Marray<double, 2> a1(dim1, dim2), b1(dim1, dim2), c1(dim1, dim2);
-
+static void addSecondRankTensors(int dim1, int dim2,int num) {
+	Marray<float, 2> a(dim1, dim2), sum(dim1, dim2);
 	Index<'i'> i;
 	Index<'j'> j;
 	for (int ind1 = 0; ind1 < dim1; ind1++) {
 		for (int ind2 = 0; ind2 < dim2; ind2++) {
-			input >> a1(ind1, ind2);
+			input >> sum(ind1, ind2);
 		}
 	}
-	for (int ind1 = 0; ind1 < dim1; ind1++) {
-		for (int ind2 = 0; ind2 < dim2; ind2++) {
-			input >> b1(ind1, ind2);
+	num--;
+	for(int ind=0;ind<num;ind++){
+		for (int ind1 = 0; ind1 < dim1; ind1++) {
+			for (int ind2 = 0; ind2 < dim2; ind2++) {
+				input >> a(ind1, ind2);
+			}
 		}
+		sum(i,j) = sum(i,j) + a(i, j);
 	}
 
-	c1(i,j) = a1(i,j) + b1(i, j);
-
 	for (int ind1 = 0; ind1 < dim1; ind1++) {
 		for (int ind2 = 0; ind2 < dim2; ind2++) {
-			output << c1(ind1, ind2) << " ";
+			output << sum(ind1, ind2) << " ";
 		}
 	}
 }
 
 
 
-static void addThirdRankTensors(int dim1, int dim2, int dim3) {
-	Marray<double, 3> a1(dim1, dim2, dim3), b1(dim1, dim2, dim3), c1(dim1, dim2, dim3);
+static void addThirdRankTensors(int dim1, int dim2, int dim3, int num) {
+	Marray<float, 3> a(dim1, dim2, dim3), sum(dim1, dim2, dim3);
 
 	Index<'i'> i;
 	Index<'j'> j;
@@ -73,23 +73,25 @@ static void addThirdRankTensors(int dim1, int dim2, int dim3) {
 	for (int ind1 = 0; ind1 < dim1; ind1++) {
 		for (int ind2 = 0; ind2 < dim2; ind2++) {
 			for (int ind3 = 0; ind3 < dim3; ind3++) {
-				input >> a1(ind1, ind2, ind3);
+				input >> sum(ind1, ind2, ind3);
 			}
 		}
+	}
+	num--;
+	for(int ind=0;ind<num;ind++){
+		for (int ind1 = 0; ind1 < dim1; ind1++) {
+			for (int ind2 = 0; ind2 < dim2; ind2++) {
+				for (int ind3 = 0; ind3 < dim3; ind3++) {
+					input >> a(ind1, ind2, ind3);
+				}
+			}
+		}
+		sum(i, j, k) = sum(i, j, k) + a(i, j, k);
 	}
 	for (int ind1 = 0; ind1 < dim1; ind1++) {
 		for (int ind2 = 0; ind2 < dim2; ind2++) {
 			for (int ind3 = 0; ind3 < dim3; ind3++) {
-				input >> b1(ind1, ind2, ind3);
-			}
-		}
-	}
-	c1(i, j, k) = a1(i, j, k) + b1(i, j, k);
-
-	for (int ind1 = 0; ind1 < dim1; ind1++) {
-		for (int ind2 = 0; ind2 < dim2; ind2++) {
-			for (int ind3 = 0; ind3 < dim3; ind3++) {
-				output << c1(ind1, ind2, ind3)  << " ";
+				output << sum(ind1, ind2, ind3)  << " ";
 			}
 		}
 	}
@@ -97,47 +99,52 @@ static void addThirdRankTensors(int dim1, int dim2, int dim3) {
 
 
 void tensorAddLTensor() {
-	int rank, dim1, dim2, dim3;
+	int num, rank, dim1, dim2, dim3;
 
-//	input.open("test/test1_in1.txt", fstream::out);
-//	input << 1 << endl;
-//	input << 1000000 << endl;
-//	for(long i=0;i<1000000;i++){
-//		input << generateRandomNumber() << " ";
-//	}
-//	input << "\n";
-//	for(long i=0;i<1000000;i++){
-//		input << generateRandomNumber() << " ";
-//	}
-//	input.close();
-//	return;
+	//	input.open("test/test1_in1.txt", fstream::out);
+	//	input << 1 << endl;
+	//	input << 1000000 << endl;
+	//	for(long i=0;i<1000000;i++){
+	//		input << generateRandomNumber() << " ";
+	//	}
+	//	input << "\n";
+	//	for(long i=0;i<1000000;i++){
+	//		input << generateRandomNumber() << " ";
+	//	}
+	//	input.close();
+	//	return;
 
 	input.open("test/test3_in.txt", fstream::in);
 	if(!input){
 		cout << "Input file does not exists!\n";
 		return;
 	}
-	output.open("test/test31_out.txt", fstream::out);
+	output.open("test/test3_out.txt", fstream::out);
+	input >> num;
 	input >> rank;
 
 	switch (rank) {
-	case 0:
+	case 0: {
 		//tensor rank 0: scalar, trivial case
-		double scalar1, scalar2;
-		input >> scalar1 >> scalar2;
-		output << scalar1 + scalar2;
+		float sum=0, scalar;
+		for(int i=0;i<num;i++){
+			input >> scalar;
+			sum+=scalar;
+		}
+		output << sum;
 		break;
+	}
 	case 1:
 		input >> dim1;
-		addFirstRankTensors(dim1);
+		addFirstRankTensors(dim1,num);
 		break;
 	case 2:
 		input >> dim1 >> dim2;
-		addSecondRankTensors(dim1, dim2);
+		addSecondRankTensors(dim1, dim2,num);
 		break;
 	case 3:
 		input >> dim1 >> dim2 >> dim3;
-		addThirdRankTensors(dim1, dim2, dim3);
+		addThirdRankTensors(dim1, dim2, dim3,num);
 		break;
 	default:
 		cout << "Ranks higher than 3 are not supported yet\n";
