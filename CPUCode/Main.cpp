31dcca4044e28ad860fccb30614b9715bc2c0e55
djpesic <djpesic@yahoo.com>
2016-03-13 23:52:34 +0100

    Improve time measurement to ms resolution

--- CPUCode/Main.cpp
+++ CPUCode/Main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char** argv){
 	inputName=(char*)"test/test1_in.txt";
 	outputName=(char*)"test/test1_out.txt";
 	srand(time(NULL));
-		runFile(inputName, outputName);
-//	runRandomMode();
+//		runFile(inputName, outputName);
+	runRandomMode();
 	return 0;
 }
